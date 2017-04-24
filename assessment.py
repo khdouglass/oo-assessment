2bb719b99fb67570d577d249f50c6317ea21a4eb
vagrant <vagrant@vagrant.vm>
2017-04-24 16:52:36 +0000

    Made corrections to assessment.

--- assessment.py
+++ assessment.py
@@ -56,6 +56,7 @@ Part 1: Discussion
 """Part 2"""
 
 class Student(object):
+    """Student."""
 
     def __init__(self, first_name, last_name, address):
         self.first_name = first_name
@@ -68,6 +69,7 @@ class Student(object):
 
 
 class Question(object):
+    """ Question in an exam."""
 
     def __init__(self, question, correct_answer):
         self.question = question
@@ -77,13 +79,14 @@ class Question(object):
         return '({} {})'.format(self.question, self.correct_answer)
 
     def ask_and_evaluate(self):
-        """Prompt question to console and evaulate user answer"""
+        """Show question to user and evaulate user answer"""
 
-        user_anwer = raw_input(self.question)
+        user_anwer = raw_input(self.question + ">")
         return user_anwer == self.correct_answer
 
 
 class Exam(object):
+    """A test comprised of zero or more questions."""
 
     def __init__(self, name):
         self.name = name
@@ -93,20 +96,22 @@ class Exam(object):
         return '(Exam {})'.format(self.name)
 
     def add_question(self, question, correct_answer):
-        """Create question instance"""
+        """Create question instance. Add to exam."""
 
-        self.new_question = Question(question, correct_answer)
-        self.question_list.append(self.new_question)
+        new_question = Question(question, correct_answer)
+        self.question_list.append(new_question)
 
     def administer(self):
         """Administer exam's questions and returns user's score"""
+
         correct_counter = 0
         for question in self.question_list:
-            if Question.ask_and_evaluate(question) == True:
+            if question.ask_and_evaluate():
                 correct_counter += 1
-        return (float(correct_counter) / (len(self.question_list) / 2)) * 100       
+        return float(correct_counter) / len(self.question_list) * 100       
 
 class StudentExam(object):
+    """An exam belonging to a particular student. Contains student's score."""
 
     def __init__(self, student, exam):
         self.student = student
@@ -114,62 +119,82 @@ class StudentExam(object):
         self.student_score = None
 
     def __repr__(self):
-        return '(StudentExam {})'.format(self.name)
+        return '(StudentExam for {}, test: {}, score: {}.)'.format(self.student, 
+                                                            self.exam, 
+                                                            self.student_score)
 
     def take_test(self):
         """Administer exam and assign score"""
+
         self.student_score = self.exam.administer()
-        print "Your score is {}".format(self.student_score)
+
+        print "Your score is {:.2f} percent".format(self.student_score)
 
 class Quiz(Exam):
-    
-    def __init__(self):
-      self.student_score = None
+        """Quizzes for students.
 
+    A quiz is like an exam, except that it is pass/fail.
+    A quiz passes if more than 50% of the answers are correct.
+    """
+  
     def __repr__(self):
       return '(Quiz {})'.format(self.name)
     
     def administer(self):
         """Administer exam questions and return 1 or 0"""
-        correct_counter = 0
-        for question in self.question_list:
-            if Question.ask_and_evaluate(question) == True:
-                correct_counter += 1
-        if correct_counter / len(question_list) >= .5:
+        
+        score = super(Quiz, self).administer()
+
+        if score >= 0.5:
             return 1
         else:
             return 0
+
             
-class StudentQuiz(StudentExam):
+class StudentQuiz(object):
+    """A quiz belonging to a particular student. Contains score for quiz."""
 
-    def __init__(quiz):
+    def __init__(self, student, quiz):
         self.quiz = quiz
+        self.student = student
+        self.score = None
 
-    def __repr__(self):
-        return '(StudentQuiz {})'.format(self.name)
+        def __repr__(self):
+
+        template = "<StudentQuiz student: {}, exam: {}, score: {}>"
+
+        return template.format(self.student.first_name + " " + self.student.last_name,
+                               self.quiz.name,
+                               self.score)
+                                                            )
 
     def take_test(self):
-        """Administer exam and assign score"""
+        """Administer exam and return score"""
+
         self.student_score = self.quiz.administer()
-        if self.student_score == 1:
+
+        if self.student_score:
             print "You passed!"
         else:
             print "You failed."
 
 
-def example(exam_name, first_name, last_name, student_address):
-    """Create exam and administer to student"""
+def example():
+    """Show usage of exam, questions, and student."""
 
-    exam_name = Exam(exam_name)
+    exam_name = Exam('midterm')
+
     exam_name.add_question("What is the color of the sky? ", "blue")
+
     exam_name.add_question("How many days are in a year? ", 365)
+
     exam_name.add_question("Do dolphins live in the ocean? ", "yes")
     
-    student_name = Student(first_name, last_name, student_address)
+    student_name = Student("Joe", "Computer", "555 California Street")
 
-    student_exam = StudentExam(student_name, exam_name)
+    joe_midterm = StudentExam(student, exam)
 
-    student_exam.take_test()
+    joe_midterm.take_test()
     
 
 
